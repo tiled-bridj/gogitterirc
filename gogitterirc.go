a9b3a86df3864095dc3f29ee02d55a03bc965fc6
mrexodia <mr.exodia.tpodt@gmail.com>
2016-03-17 15:51:40 +0100

    added JSON config (with good error reporting)

--- gogitterirc.go
+++ gogitterirc.go
@@ -2,32 +2,46 @@ package main
 
 import (
 	"fmt"
-	/* "github.com/sromku/go-gitter" */
+	"github.com/jinzhu/configor"
+	/*"github.com/sromku/go-gitter"*/
 	"github.com/thoj/go-ircevent"
 )
 
+type Config struct {
+	IRC struct {
+		Server  string `default:"irc.freenode.net:6667"`
+		Nick    string `required:"true"`
+		Channel string `required:"true"`
+	}
+	Gitter struct {
+		Apikey string `required:"true"`
+		Room   string `required:"true"`
+	}
+}
+
 func main() {
 	fmt.Println("Gitter/IRC Sync Bot, written in Go by mrexodia")
+	var conf Config
+	if err := configor.Load(&conf, "config.json"); err != nil {
+		fmt.Printf("Error loading config: %v...\n", err)
+		return
+	}
 
-	ircNick := "gogitterirc"
-	ircServ := "irc.freenode.net:6667"
-	ircChan := "#x64dbg"
-	ircCon := irc.IRC(ircNick, ircNick)
-	ircErr := ircCon.Connect(ircServ)
-	if ircErr != nil {
-		fmt.Printf("Failed to connect to %v as %v", ircServ, ircNick)
+	ircCon := irc.IRC(conf.IRC.Nick, conf.IRC.Nick)
+	if err := ircCon.Connect(conf.IRC.Server); err != nil {
+		fmt.Printf("Failed to connect to %v: %v...\n", conf.IRC.Server, err)
 		return
 	}
 	ircCon.AddCallback("001", func(e *irc.Event) {
-		ircCon.Join(ircChan)
+		ircCon.Join(conf.IRC.Channel)
 	})
 	ircCon.AddCallback("JOIN", func(e *irc.Event) {
-		ircCon.Privmsg(ircChan, "Hello, I'll be syncronizing between IRC and Gitter today!")
+		ircCon.Privmsg(conf.IRC.Channel, "Hello, I'll be syncronizing between IRC and Gitter today!")
 	})
 	ircCon.AddCallback("PRIVMSG", func(e *irc.Event) {
 		gitterMsg := fmt.Sprintf("<%v> %v", e.Nick, e.Message())
-		fmt.Printf("[IRC] %v", gitterMsg)
-		ircCon.Privmsg(ircChan, gitterMsg)
+		fmt.Printf("[IRC] %v\n", gitterMsg)
+		ircCon.Privmsg(conf.IRC.Channel, gitterMsg)
 	})
 	ircCon.Loop()
 }
